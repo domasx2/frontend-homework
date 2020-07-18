139130737733f1a3cb0ec9d154572af58689efdc
Domas <domasx2@gmail.com>
2020-07-18 23:06:34 +0300

    the build

---
+++ precache-manifest.e05e687d94715874513657f5341871fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3804074f91aa6a8b949975c3126ce665",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5bb1366e614df9ecd6f",
+    "url": "/static/css/main.d5cb7e89.chunk.css"
+  },
+  {
+    "revision": "189bd246cbd68e4b3cc1",
+    "url": "/static/js/2.1a532120.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a532120.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5bb1366e614df9ecd6f",
+    "url": "/static/js/main.b892d4c7.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
